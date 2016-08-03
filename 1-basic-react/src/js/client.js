66880bc848d31e7cbbac561396595037ef30a6cb
sodacarbon <sodacarbon@gmail.com>
2016-08-03 22:35:34 +0700

    vid#2.1

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
+
+class Layout extends React.Component {
+    val1 = 'Will';
+
+    render() {
+        return (
+          <h1>It's {this.val1}!</h1>
+        );
+    }
+}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
